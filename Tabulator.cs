1d599263a6e993d2f467a0254083e6985d30a28b
Rutger Farry <rutgerfarry@gmail.com>
2016-03-15 13:28:26 -0700

    Remove unused imports

--- Tabulator.cs
+++ Tabulator.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Xml;
 using System.Text.RegularExpressions;
-using System.Diagnostics;
 
 namespace TabulateSmarterTestContentPackage
 {
