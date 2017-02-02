e94674b964a31ede72c91e70f7bed8389b769447
Gregory White <hbgwhite@hotmail.com>
2017-02-01 20:46:32 -0800

    Develop (#4)
    * Project Reorganization
    
    * SMD-1: Added Depth of Knowledge XML retrieval and removed unused imports to Tabulator.cs
    
    * SMD-1: Added "DOK" column to item report and added code to write DOK values to this new column. Default to empty if DOK is not present for that item
    
    * SMD-2: Allow Calculator metadata field added to the item report. Producing an error if the subject is "MATH" and the AllowCalculator field is not present
    
    * SMD-3: img tag alt attribute checks
    
    * Added TabulateSmarterTestContentPackage.Tests and eliminated merge errors

--- TabulateSmarterTestContentPackage.Tests/Properties/AssemblyInfo.cs
+++ TabulateSmarterTestContentPackage.Tests/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TabulateSmarterTestContentPackage")]
-[assembly: AssemblyDescription("See https://github.com/SmarterApp/TabulateSmarterTestContentPackage")]
+[assembly: AssemblyTitle("TabulateSmarterTestContentPackage.Tests")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TabulateSmarterTestContentPackage")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("TabulateSmarterTestContentPackage.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("56aba4c2-1a1a-4ad2-a164-4cfc8cfcea91")]
+[assembly: Guid("eb026227-bba6-4adc-b687-bbe0ac39d58e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.0.0")]
-[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
