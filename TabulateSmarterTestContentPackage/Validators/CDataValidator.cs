75e09bea394464820d1f5c01688b1030e10f20dc
Gregory White <gwhite@fairwaytech.com>
2017-06-20 18:11:13 -0700

    Added support for discovering color changing elements and css

--- TabulateSmarterTestContentPackage/Validators/CDataValidator.cs
+++ TabulateSmarterTestContentPackage/Validators/CDataValidator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 using System.Xml.XPath;
@@ -26,15 +27,98 @@ namespace TabulateSmarterTestContentPackage.Validators
                 // use a global selector.
                 var imgTags = cDataSection.XPathSelectElements("//img");
 
-                // Right now, this is the only validity check we're doing on CData. This will be expanded
-                // as more validations become necessary.
-                return imgTags.All(ImgElementHasValidAltTag);
+                return imgTags.All(ImgElementHasValidAltTag)
+                       && ElementsFreeOfColorAlterations(cDataSection.Root)
+                       && ElementsFreeOfViolatingStyleTags(cDataSection.Root, new List<string>
+                       {
+                           "color",
+                           "background",
+                           "background-color",
+                           "border",
+                           "border-bottom-color",
+                           "border-color",
+                           "border-left",
+                           "border-left-color",
+                           "border-right",
+                           "border-right-color",
+                           "border-top",
+                           "box-shadow",
+                           "column-rule",
+                           "column-rule-color",
+                           "filter",
+                           "font",
+                           "opacity",
+                           "outline",
+                           "outline-color",
+                           "text-decoration",
+                           "text-decoration-color",
+                           "text-shadow"
+                       });
             }
             catch (Exception ex)
             {
                 Logger.Error(ex.Message);
             }
             return false;
+        }
+
+        public static bool ElementsFreeOfColorAlterations(XElement rootElement)
+        {
+            if (rootElement == null)
+            {
+                Logger.Error("CData section provided to color validation does not have valid content.");
+                return false;
+            }
+            var restrictedValues = new List<string>
+            {
+                "color",
+                "bgcolor"
+            };
+            return restrictedValues.All(x => ReportElementsInViolation(rootElement, "//*", x));
+        }
+
+        public static bool ReportElementsInViolation(XElement rootElement, string path, string attribute)
+        {
+            var result = rootElement.ElementsByPathAndAttributeCaseInsensitive(path, attribute).ToList();
+            if (!result.Any())
+            {
+                return true;
+            }
+            result.ForEach(
+                x =>
+                {
+                    Logger.Error(
+                        $"CData element {x.Name.LocalName} matches an illegal pattern: {path}[@{attribute.ToLower()}]");
+                });
+            return false;
+        }
+
+        public static bool ElementsFreeOfViolatingStyleTags(XElement rootElement, IEnumerable<string> restrictedCss)
+        {
+            var result = rootElement.ElementsByPathAndAttributeCaseInsensitive("//*", "style").ToList();
+            var isValid = true;
+            if (result.Any())
+            {
+                restrictedCss.ToList().ForEach(x =>
+                    result.ToList().Where(y => y.Attributes()
+                        .First(z => z.Name.LocalName.Equals("style", StringComparison.OrdinalIgnoreCase))
+                        .Value.Equals(x, StringComparison.OrdinalIgnoreCase)).ToList().ForEach(y =>
+                    {
+                        var errorText =
+                            $"Element {y.Name.LocalName} in CData contains an illegal CSS marker {x} in its 'style' attribute";
+                        if (x.ToLower().Contains("color"))
+                        {
+                            Logger.Error(errorText);
+                        }
+                        else
+                        {
+                            Logger.Warn(errorText);
+                        }
+                        isValid = false;
+                    })
+                );
+            }
+            return isValid;
         }
 
         //<summary>This method takes a <img> element tag and determines whether
