1376d6f0dae75eaa2cb3ed7eda2cde29a8c948bb
Gregory White <gwhite@fairwaytech.com>
2017-06-28 10:14:39 -0700

    Ensure no short circuiting for validation calls

--- TabulateSmarterTestContentPackage/Validators/CDataValidator.cs
+++ TabulateSmarterTestContentPackage/Validators/CDataValidator.cs
@@ -30,35 +30,25 @@ namespace TabulateSmarterTestContentPackage.Validators
                 // use a global selector.
                 var imgTags = cDataSection.XPathSelectElements("//img");
                 var imgTagsValid = imgTags.Select(x => ImgElementHasValidAltTag(x, itemContext)).ToList();
+                var noColorAlterations = ElementsFreeOfColorAlterations(cDataSection.Root, itemContext);
+                var noViolatingStyleTags = ElementsFreeOfViolatingStyleTags(cDataSection.Root, new List<string>
+                {
+                    "color",
+                    "background",
+                    "border",
+                    "box-shadow",
+                    "column-rule",
+                    "filter",
+                    "font",
+                    "opacity",
+                    "outline",
+                    "text-decoration",
+                    "text-shadow"
+                }, itemContext);
 
                 return imgTagsValid.All(x => x)
-                       && ElementsFreeOfColorAlterations(cDataSection.Root, itemContext)
-                       && ElementsFreeOfViolatingStyleTags(cDataSection.Root, new List<string>
-                       {
-                           "color",
-                           "background",
-                           //"background-color",
-                           "border",
-                           //"border-bottom-color",
-                           //"border-color",
-                           //"border-left",
-                           //"border-left-color",
-                           //"border-right",
-                           //"border-right-color",
-                           //"border-top",
-                           //"border-top-color",
-                           "box-shadow",
-                           "column-rule",
-                           //"column-rule-color",
-                           "filter",
-                           "font",
-                           "opacity",
-                           "outline",
-                           //"outline-color",
-                           "text-decoration",
-                           //"text-decoration-color",
-                           "text-shadow" //select-color, padding TODO: Greg - condense this list to common terms 'background' etc and only throw 1 error per item max
-                       }, itemContext);
+                       && noColorAlterations
+                       && noViolatingStyleTags;
             }
             catch (Exception ex)
             {
