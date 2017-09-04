cf387e904b4288b14c60dfa3e6459977df3c78c9
Greg White <gwhite@fairwaytech.com>
2017-09-04 12:12:59 -0700

    removed extra docker files, added sorting to return to API, switched disk folders

--- ContentPackageTabulator.Web/Controllers/ValidationController.cs
+++ ContentPackageTabulator.Web/Controllers/ValidationController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using ContentPackageTabulator;
 using Microsoft.AspNetCore.Mvc;
 using ContentPackageTabulator.Web.Models;
+using System;
 
 namespace TabulateSmarterTestContentPackage.Web.Controllers
 {
@@ -19,7 +20,51 @@ namespace TabulateSmarterTestContentPackage.Web.Controllers
                 Detail = x.Detail,
                 Message = x.Message,
                 Severity = x.Severity.ToString()
-            });
+            }).OrderBy(x => x.Severity, new CustomStringComparer(StringComparer.OrdinalIgnoreCase))
+                         .ThenBy(x => x.Category)
+                         .ThenBy(x => x.Message)
+                         .ThenBy(x => x.Detail);
         }
     }
+
+	class CustomStringComparer : IComparer<string>
+	{
+		private readonly IComparer<string> _baseComparer;
+		public CustomStringComparer(IComparer<string> baseComparer)
+		{
+			_baseComparer = baseComparer;
+		}
+
+		public int Compare(string x, string y)
+		{
+			if (_baseComparer.Compare(x, y) == 0)
+				return 0;
+
+			// "severe" comes before everything else
+			if (_baseComparer.Compare(x, "Severe") == 0)
+				return -1;
+			if (_baseComparer.Compare(y, "Severe") == 0)
+				return 1;
+
+			// "degraded" comes next
+			if (_baseComparer.Compare(x, "Degraded") == 0)
+				return -1;
+			if (_baseComparer.Compare(y, "Degraded") == 0)
+				return 1;
+
+			// "tolerable" comes next
+			if (_baseComparer.Compare(x, "Tolerable") == 0)
+				return -1;
+			if (_baseComparer.Compare(y, "Tolerable") == 0)
+				return 1;
+
+			// "benign" comes last
+			if (_baseComparer.Compare(x, "Benign") == 0)
+				return -1;
+			if (_baseComparer.Compare(y, "Benign") == 0)
+				return 1;
+
+			return _baseComparer.Compare(x, y);
+		}
+	}
 }
